75384868da786ace6b21d6434fbef2c57d1b1338
Alkawe <alixkwebb@gmail.com>
2020-09-02 23:08:27 -0400

    adding initial files

---
+++ script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialChar = ["!","@","#","$"];
+var number = [1,2,3,4,5,6,7,8,9];
+
+var length = prompt("Please enter the length you want your password to be, from 8 - 128 characters");
+
+
+// Write password to the #password input
+function writePassword() {
+  // var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = "Learning javascript";
+  // passwordText.value = password;
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
