35d907dc12e042b5dac0c94237c559dc5354cad0
Alkawe <alixkwebb@gmail.com>
2020-09-06 18:49:51 -0400

    added generatePassword function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordResult = document.querySelector("#password");
+var passwordText = document.querySelector("#password");
 
 // Variables for arrays
 const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -8,13 +8,23 @@ const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","
 const specialChar = ["~","!","@","#","$","%","^","&","*","(",")","-",",","+","{","}"];
 const number = [1,2,3,4,5,6,7,8,9];
 
-// Variables retreived through prompts
+// Write password to the #password input
+function writePassword() {
+    password = generatePassword();
+      
+    console.log(password);
+    passwordText.value = password;
+    return;
+}
+
+function generatePassword() {
+    // Variables retreived through prompts
 var pwLength = prompt("Please enter the length you want your password to be, from 8 - 128 characters");
-  // if length is not between 8 & 128, alert the user.
-  if (isNaN(pwLength) == true || pwLength < 8 || pwLength > 128) {
-    alert("ERROR: Please resubmit with a valid password character length (a number 8 - 128)");
-    console.log("length");
-  }
+// if length is not between 8 & 128, alert the user.
+if (pwLength < 8 || pwLength > 128) {
+  alert("ERROR: Please resubmit with a valid password character length (a number 8 - 128)");
+  console.log("length");
+}
 
 var uppercaseConfirm = confirm("Do you want uppercase letters in your password?  Select OK for yes, Cancel for no.");
 var lowercaseConfirm = confirm("Do you want lowercase letters in your password?  Select OK for yes, Cancel for no.");
@@ -24,50 +34,51 @@ var charArray = [];
 
 // If the user confirmed, add to one large array that holds all possible characters
 if (uppercaseConfirm) {
-  console.log("uppercase");
-  charArray = charArray.concat(uppercase);
-  console.log(charArray);
+console.log("uppercase");
+charArray = charArray.concat(uppercase);
+console.log(charArray);
 }
 
 if (lowercaseConfirm) {
-  console.log("lowercase");
-  charArray = charArray.concat(lowercase);
-  console.log(charArray);
+console.log("lowercase");
+charArray = charArray.concat(lowercase);
+console.log(charArray);
 }
 
 if (numericConfirm) {
-  console.log("numeric");
-  charArray = charArray.concat(number);
-  console.log(charArray);
+console.log("numeric");
+charArray = charArray.concat(number);
+console.log(charArray);
 }
 
 if (specialConfirm) {
-  console.log("special");
-  charArray = charArray.concat(specialChar);
-  console.log(charArray);
+console.log("special");
+charArray = charArray.concat(specialChar);
+console.log(charArray);
 }
 
 // Ensure at least one character type was chosen by the user.
 if (uppercaseConfirm == false && lowercaseConfirm == false && numericConfirm == false && specialConfirm == false) {
-  alert("You must select at least one character type.");
+alert("You must select at least one character type.");
+}
+    
+    // Run for loop to randomly generate characters
+    for (var i = 0; i < pwLength; i++) {
+        var randomChar = "";
+        // Randomly pull a char from the huge array of all char options
+        randomChar = charArray[Math.floor(Math.random()*charArray.length)];
+        console.log(randomChar);
+        
+        var randomPassword = "";
+        randomPassword = randomPassword.concat(randomChar);
+        // randomPassword = randomPassword.concat(charArray[Math.floor(Math.random()*charArray.length)]);
+        // alternate code: randomPassword += randomChar;
+        console.log(randomPassword);
+    }
+    return randomPassword;
 }
 
-// When the generate button is clicked, run the generatePassword and writePassword functions.
-generateBtn.addEventListener("click", writePassword());
 
+  // When the generate button is clicked, run the generatePassword and writePassword functions.
+generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
-function writePassword() {
-  // Run for loop to randomly generate characters
-  for (var i = 0; i < pwLength; i++) {
-    var randomChar = 0;
-    // Randomly pull a char from the huge array of all char options
-    randomChar = charArray[Math.floor(Math.random()*charArray.length)];
-    console.log(randomChar);
-
-    var randomPassword;
-    randomPassword += randomChar;
-    console.log(randomPassword);
-    passwordResult.value = randomPassword;
-  }
-}
